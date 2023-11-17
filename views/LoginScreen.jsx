cdbdc9f1cfdcdbb664b202e1fd7d1dbddaa7299a
Alejandra Alaniz <dominguezale377@gmail.com>
2023-11-17 11:18:08 -0300

    cambios de modelo de spinner.

--- views/LoginScreen.jsx
+++ views/LoginScreen.jsx
@@ -7,7 +7,6 @@ import Swal from 'sweetalert2';
 import { useNavigate } from "react-router-dom";
 import { login } from "../src/api/auth";
 import { useState } from "react";
-import Spinner from 'react-bootstrap/Spinner';
 import '../src/css/login.css';
 
 
@@ -52,7 +51,7 @@ const LoginScreen = () => {
       });
     }
   };
-  
+
 
 
 
@@ -156,12 +155,27 @@ const LoginScreen = () => {
       {loading && (
         <div className="position-fixed top-0 start-0 end-0 bottom-0 d-flex justify-content-center align-items-center">
           <div className="overlay"></div>
-          <div className="text-center bg-light p-3">
-            <Spinner animation="grow" className="m-2" variant="primary" role="status"></Spinner>
-            <Spinner animation="grow" className="m-2" variant="primary" role="status"></Spinner>
-            <Spinner animation="grow" className="m-2" variant="primary" role="status"></Spinner>
-            <Spinner animation="grow" className="m-2" variant="primary" role="status"></Spinner>
-            <h2 className="d-flex justify-content-around text-info fw-bold">CARGANDO...</h2>
+          <div className="text-center  p-3">
+            {/* **** */}
+            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+              <div className="wheel"></div>
+              <div className="hamster">
+                <div className="hamster__body">
+                  <div className="hamster__head">
+                    <div className="hamster__ear"></div>
+                    <div className="hamster__eye"></div>
+                    <div className="hamster__nose"></div>
+                  </div>
+                  <div className="hamster__limb hamster__limb--fr"></div>
+                  <div className="hamster__limb hamster__limb--fl"></div>
+                  <div className="hamster__limb hamster__limb--br"></div>
+                  <div className="hamster__limb hamster__limb--bl"></div>
+                  <div className="hamster__tail"></div>
+                </div>
+              </div>
+              <div className="spoke"></div>
+            </div>
+            <h2 className="d-flex justify-content-around text-black mt-2 fw-bold">CARGANDO...</h2>
           </div>
         </div>
       )}
