4b669557bdc8419b3d831eebfa584bfaca8346d4
Abigail Bruno <abigailbruno2005@gmail.com>
2023-11-27 02:19:57 -0300

    alt a imagenes de descripcion de planes

--- src/views/PlansScreen.jsx
+++ src/views/PlansScreen.jsx
@@ -1,92 +1,3 @@
-
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarApp from '../components/NavBarApp'
-import imgMainPlans from "../assets/BronzeMain1.jpg"
-import imgSilverPlans from "../assets/PlanSilver.jpg"
-import imgBronzePlans from "../assets/PlanBronze.png";
-import imgGoldPlans from "../assets/PlanGold.png";
-import "../css/plans.css"
-import FormPlans from '../components/FormPlans';
-import FooterApp from '../components/FooterApp'
-
-
-
-const PlansScreen= () =>  {
-  return (
-    <div>
-<div>
-  <div className="position-relative mx-auto img-main-plans ">
-  <img src={imgMainPlans} className="img-fluid" alt="..."/>
-  <div className="position-absolute top-0 star-0">
-  <h1 className='h1-plans'>Planes que se adaptan a tus necesidades!</h1>
-  </div>
-</div>
-</div>
-
-
-<div className='container'>
- <div className="card-group">
-  <div className="card cardPlans">
-      <img className="card-img imgPlans mx-auto" src={imgBronzePlans} alt="" />
-    <div className="card-body">
-      <h5 className="card-title ">Bronce</h5>
-      <ul className='list-group'>
-      <li className='list-group-item'>Una mascota</li>
-      <li className='list-group-item'>15% de descuento en productos</li>
-      <li className='list-group-item'>Servicios veterinarios de emergencia</li>
-      <li className='list-group-item'>Consultas online 24hs</li>
-      </ul>
-    </div>
-  </div>
-
-
-  <div className="card cardPlans">
-     <img className="card-img imgPlans mx-auto" src={imgSilverPlans} alt="" />
-    <div className="card-body">
-      <h5 className="card-title">Plata</h5>
-      <ul className='list-group'>
-      <li className='list-group-item'>Hasta tres mascotas</li>
-      <li className='list-group-item'>25% de descuento en cirujias</li>
-      <li className='list-group-item'>Servicios veterinarios de emergencia</li>
-      <li className='list-group-item'> Consultas online 24hs</li>
-      </ul>
-    </div>
-  </div>
-  
-
-
-  <div className="card cardPlans">
-     <img className="card-img imgPlans mx-auto" src={imgGoldPlans} alt="" />
-    <div className="card-body">
-      <h5 className="card-title">Oro</h5>
-      <ul className='list-group'>
-      <li className='list-group-item'>Más de tres mascotas</li>
-      <li className='list-group-item'>35% de descuento en alimentos</li>
-      <li className='list-group-item'>Servicios veterinarios de emergencia</li>
-      <li className='list-group-item'> Consultas online 24hs</li>
-      </ul>
-    </div>
-  </div>
-</div>
-</div>
-<hr className="border-bottom"/>
-<hr className="border-bottom"/>
-
-<FormPlans/>
-
-
-
-</div>
-
-
-  )
-};
-
-export default PlansScreen;
-
-
-=======
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import imgMainPlans from "../assets/BronzeMain1.jpg";
@@ -99,9 +10,9 @@ const PlansScreen = () => {
   return (
     <div>
       <div>
-        <div class="position-relative mx-auto img-main-plans ">
-          <img src={imgMainPlans} className="img-fluid" alt="..." />
-          <div class="position-absolute top-0 star-0">
+        <div className="position-relative mx-auto img-main-plans ">
+          <img src={imgMainPlans} className="img-fluid" alt="Perro blanco y negro de portada" />
+          <div className="position-absolute top-0 star-0">
             <h1 className="h1-plans">
               Planes que se adaptan a tus necesidades!
             </h1>
@@ -115,7 +26,7 @@ const PlansScreen = () => {
             <img
               className="card-img imgPlans mx-auto"
               src={imgBronzePlans}
-              alt=""
+              alt="Huella de animal"
             />
             <div className="card-body">
               <h5 className="card-title ">Bronce</h5>
@@ -136,7 +47,7 @@ const PlansScreen = () => {
             <img
               className="card-img imgPlans mx-auto"
               src={imgSilverPlans}
-              alt=""
+              alt="figura perro y gato"
             />
             <div className="card-body">
               <h5 className="card-title">Plata</h5>
@@ -157,7 +68,7 @@ const PlansScreen = () => {
             <img
               className="card-img imgPlans mx-auto"
               src={imgGoldPlans}
-              alt=""
+              alt="figura animales varios"
             />
             <div className="card-body">
               <h5 className="card-title">Oro</h5>
