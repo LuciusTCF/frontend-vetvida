75930e8848c767668a9cd207a6b6c8ac2d968ec3
Lux <lucaschcobarferreyra@gmail.com>
2023-12-03 17:32:40 -0300

    Arreglados problemas de rutas

--- src/views/ErrorScreen.jsx
+++ src/views/ErrorScreen.jsx
@@ -1,20 +1,19 @@
-import Lottie from 'lottie-react';
-import errorAnimation from '../assets/error404.json';
+import Lottie from "lottie-react";
+import errorAnimation from "../assets/error404.json";
 import "../css/login.css";
 import { Link } from "react-router-dom";
 
-
 const ErrorScreen = () => {
-    return (
-        <div className="error404-container m-auto vh-100">
-        <Lottie animationData={errorAnimation} />
-        <div className="d-block">
-        <Link to='/' className='btn btn-success'><i className="bi bi-house-fill"></i> Volver</Link>
-        </div>
+  return (
+    <div className="error404-container m-auto vh-100">
+      <Lottie animationData={errorAnimation} />
+      <div className="d-block">
+        <Link to="/" className="btn btn-success">
+          <i className="bi bi-house-fill"></i> Volver
+        </Link>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default ErrorScreen;
-
-
